f3938e6e95b3ec2f356ca939206e5eea4a265a17
Pradeep Sharma <124363229+Pradeep-pareek@users.noreply.github.com>
2023-09-16 10:10:35 +0000

    new

---
+++ Javascripat/datatypes.js
@@ -0,0 +1,14 @@
+"use strict"   //treat all js code as newer version
+
+//alert(3 + 3) we are using nodejs , not browser
+
+//premative datatypes
+
+// let number = 33;
+// let string = "pradeep";
+// let bigint = BigInt(); 
+// let boolean = true; 
+// let null =  null;
+// let symbol = unique;
+// let undefined = undefined;
+
